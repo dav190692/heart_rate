a2ce60be6597c11da7ce2f657fba14cfcd987e0b
dav190692 <dav190692@gmail.com>
2023-04-05 21:39:52 +0400

    05.04.2023

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="../icons/chevron-left-solid.png" alt=""></img> </button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png" alt=""></img></button>'
+      });
+  });
