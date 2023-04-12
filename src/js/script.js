7517255fb04da676ddbc7863877a8f0e4be6c739
dav190692 <dav190692@gmail.com>
2023-04-12 12:20:08 +0400

    12.04.2023

--- src/js/script.js
+++ src/js/script.js
@@ -116,6 +116,8 @@ $(document).ready(function(){
       data: $(this).serialize()
     }).done(function(){
       $(this).find("input").val("");
+      $('consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
 
 
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = '1980har2023@gmail.com';                 // Наш логин
+$mail->Password = 'sryqwclkqxsukqpo';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('1980har2023@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('detixe4213@fitzola.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
