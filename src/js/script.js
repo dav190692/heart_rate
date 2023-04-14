18ef523f5b983c1cdfd5f659afbeacf0603ac9e4
dav190692 <dav190692@gmail.com>
2023-04-14 15:49:40 +0400

    14.04.2023-2

--- src/js/script.js
+++ src/js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
             $(this).on('click', function(e){
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__listwrap').eq(i).toggleClass('catalog-item__listwrap_active');
     
     
             })
