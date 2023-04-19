7c5b95efad6e4ba5755e76284018f8f4089968d5
dav190692 <dav190692@gmail.com>
2023-04-19 15:31:09 +0400

    19.04.2023

--- src/js/script.js
+++ src/js/script.js
@@ -9,33 +9,33 @@ $(document).ready(function(){
         dotsClass: "my-dots",
         responsive: [
             {
-              // breakpoint: 992,
-              //   settings: {
-              //       // dots: true,
-              //       // dotsClass: "my-dots",
-              //       arrows: false
-              // }
-              breakpoint: 1280,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
+              breakpoint: 992,
+                settings: {
+                    // dots: true,
+                    // dotsClass: "my-dots",
+                    arrows: false
               }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+            //   breakpoint: 1280,
+            //   settings: {
+            //     slidesToShow: 1,
+            //     slidesToScroll: 2,
+            //     infinite: true,
+            //     dots: true
+            //   }
+            // },
+            // {
+            //   breakpoint: 600,
+            //   settings: {
+            //     slidesToShow: 2,
+            //     slidesToScroll: 2
+            //   }
+            // },
+            // {
+            //   breakpoint: 480,
+            //   settings: {
+            //     slidesToShow: 1,
+            //     slidesToScroll: 1
+            //   }
             
             }]
       });
